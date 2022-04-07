0c8f853dbeb1c4975f879705138eee8d165b45da
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-06 17:27:50 -0700

    fix api

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5001/api/";
-const BASE_URL = "https://deployretry.herokuapp.com/api/";
+const BASE_URL = "http://localhost:5001/api/";
+// const BASE_URL = "https://deployretry.herokuapp.com/api/";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzY1MThhYjZhYjM3OGQ3ZmQ2ODZlNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0ODYwMTUyNSwiZXhwIjoxNjQ4ODYwNzI1fQ.Uc5SowHbvjFt0yl6txT2o28SiFyJTWT76mfozQcuH5I";
 
